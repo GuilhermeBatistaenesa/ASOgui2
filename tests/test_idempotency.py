b22c6a557b05079b452fc554ee1b1f665d64395f
Guilherme Batista <guilherme.batista@enesa.com.br>
2026-02-04 12:15:15 -0300

    chore: Remove an unspecified deleted file.

--- tests/test_idempotency.py
+++
@@ -1,8 +0,0 @@
-from idempotency import should_skip_duplicate
-
-
-def test_should_skip_duplicate(tmp_path):
-    path = tmp_path / "file.txt"
-    assert should_skip_duplicate(str(path)) is False
-    path.write_text("x", encoding="utf-8")
-    assert should_skip_duplicate(str(path)) is True
